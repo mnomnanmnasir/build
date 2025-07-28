b67861c97b1af8ab4ccf71d69d9059373a5e7c00
mnomnanmnasir <nomannasir@i8is.com>
2025-07-28 09:06:03 -0300

    updated fix error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite"
-import * as path from "path"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
   }
 })
