443049cebdbda97017237eccc7c561af71d5006a
mnomnanmnasir <nomannasir@i8is.com>
2025-07-28 09:33:47 -0300

    fixxx

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',  // Changed from 'dist' to 'build'
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
