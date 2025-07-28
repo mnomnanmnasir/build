71b83f1a6a027e5f7541e12e0b1742465d7ae743
mnomnanmnasir <nomannasir@i8is.com>
2025-07-28 09:01:01 -0300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import * as path from "path"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+  }
 })
-
